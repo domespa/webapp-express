8c1df6df7f228dbab28dfca52fe41ea902d51ad9
domespa <dumiii1988@gmail.com>
2025-02-24 16:46:53 +0100

    add env files

--- data/db.js
+++ data/db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Az123456!",
-  database: "db_movies",
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to MySQL!");
 });
+
 module.exports = connection;
